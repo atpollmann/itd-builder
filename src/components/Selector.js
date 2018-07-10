b8c80159fca2991469a71c2de4717c02ff3430fe
Augusto Toledo <atoledo@pcorp.cl>
2018-07-10 01:14:59 -0400

    Selector shape is configurable

--- src/components/Selector.js
+++ src/components/Selector.js
@@ -6,7 +6,9 @@ import {
   ITD_SELECTOR_BG_ON,
   ITD_SELECTOR_BG_OFF,
   ITD_SELECTOR_BO_ON,
-  ITD_SELECTOR_BO_OFF
+  ITD_SELECTOR_BO_OFF,
+  ITD_SELECTOR_TYPE,
+  ITD_SELECTOR_RADIUS
 } from '../config'
 
 class Selector extends Component {
@@ -24,18 +26,35 @@ class Selector extends Component {
     this.setState({ isOn: false })
     this.props.dispatch(closePopper())
   }
+  
+  baseStyle = {
+    position: 'absolute',
+    left: this.props.left,
+    top: this.props.top,
+    width: this.props.width,
+    height: this.props.height,
+  }
+  
+  boxStyle = {
+    ...this.baseStyle,
+    borderRadius: `${ITD_SELECTOR_RADIUS}px`,
+  }
+  
+  circleStyle = {
+    ...this.baseStyle,
+    width: this.props.width,
+    height: this.props.width,
+    borderRadius: '50%'
+  }
 
   render() {
+    
+    const selectorStyle = ITD_SELECTOR_TYPE === 'box' ? this.boxStyle : this.circleStyle
     
     return (
       <div
         style={{
-          position: 'absolute',
-          borderRadius: '5px',
-          left: this.props.left,
-          top: this.props.top,
-          width: this.props.width,
-          height: this.props.height,
+          ...selectorStyle,
           border: this.state.isOn ? ITD_SELECTOR_BO_ON : ITD_SELECTOR_BO_OFF,
           backgroundColor: this.state.isOn ? ITD_SELECTOR_BG_ON : ITD_SELECTOR_BG_OFF
         }}
@@ -52,7 +71,7 @@ Selector.propTypes = {
   left: PropTypes.number.isRequired,
   top: PropTypes.number.isRequired,
   width: PropTypes.number.isRequired,
-  height: PropTypes.number.isRequired
+  height: PropTypes.number
 }
 
 function mapStateToProps(state) {
--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
+export const ITD_SELECTOR_TYPE = 'box'
+export const ITD_SELECTOR_RADIUS = '5'
 export const ITD_SELECTOR_BG_ON = 'rgba(255,0,0,0.2)'
 export const ITD_SELECTOR_BG_OFF = 'rgba(0,255,0,0)'
 export const ITD_SELECTOR_BO_ON = '2px solid red'
